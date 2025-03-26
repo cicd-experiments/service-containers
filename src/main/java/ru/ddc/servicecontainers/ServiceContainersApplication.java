d932c2f543c7cab00ebbf3a4ccf05dcf45170caf
Sergey Kovalev <dipdeepcode@mail.ru>
2025-03-26 21:56:11 +0300

    init commit

---
+++ src/main/java/ru/ddc/servicecontainers/ServiceContainersApplication.java
@@ -0,0 +1,13 @@
+package ru.ddc.servicecontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceContainersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceContainersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ddc/servicecontainers/ServiceContainersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddc.servicecontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceContainersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
